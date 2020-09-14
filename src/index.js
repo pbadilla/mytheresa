549c5e4c353f5878f5590955be982f5afe567bbd
pbadilla <pachibadilla@gmail.com>
2020-09-14 08:15:45 +0200

    02 Some improvements

--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Header } from './components/Header';
-import { Home } from './components/Home';
-
-
-class App extends React.Component {
-  render() {
-    let user = {
-      name: "Luis Martinez",
-      hobbies: [
-        'soccer',
-        'web development'
-      ]
-    }
-    return (
-      <div>
-        <Header textHomeLink="Hello World" />
-        <Home
-          name={"LCM"}
-          initialAge={27}
-          user={user}
-        >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab facere accusamus a itaque harum odit, facilis sed maxime quos officiis. Eaque incidunt
-          </p>
-        </Home>
-      </div>
-    );
-  };
-};
-
-render(
-  <App/>,
-  document.getElementById('app')
+import ReactDOM from 'react-dom';
+ 
+ReactDOM.render(
+    <p>Hello world</p>,
+    document.getElementById('app')
 );
+ 
+if (module.hot) {
+  module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -16,21 +16,52 @@ module.exports = {
   },
   module: { 
     rules: [ 
-      { test: /\.js$/, 
+      { 
+        test: /\.(js|jsx)$/, 
         exclude: /(node_modules|bower_components)/, 
         use: { 
           loader: 'babel-loader', 
           options: { 
             presets: ['env', 'react'] 
-        } 
-      } 
-    }],
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        exclude: /node_modules/,
+        loaders: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: '[local]___[hash:base64:5]'
+            }
+          },
+        'sass-loader',
+        ]
+      }
+    ],
     plugins: [
       new HtmlWebpackPlugin({
         template: "./src/index.html"
       }),
       new CleanWebpackPlugin(['./dist']),
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      })
     ]
   }
 };
